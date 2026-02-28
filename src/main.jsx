e63d3f8cd2575501618800aff43a5cd627b2d5c5
Josue <josueperezv2004@gmail.com>
2026-02-28 04:26:19 +0000

    FEAT: Envolving with new themes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
 import { router } from "./app/router";
+import { theme } from "./ui/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
